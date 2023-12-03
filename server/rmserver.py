4f995eb6e32b40a6df58bc4afaf5280227cbaa5a
Ram Varra <ramvarra@hotmail.com>
2023-12-03 11:33:05 -0700

    xy

--- server/rmserver.py
+++ server/rmserver.py
@@ -3,6 +3,9 @@ import os
 
 from common import util
 
+def abcXyz():
+    return 10
+
 if __name__ == '__main__':
     print("Util Version: ", util.version())
     for var, value in os.environ.items():
