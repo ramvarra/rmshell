fd1dfccff21774af4c57b0b49542d131bcb5cb86
Ram Varra <ramvarra@hotmail.com>
2023-12-03 11:24:31 -0700

    upd2

--- server/rmserver.py
+++ server/rmserver.py
@@ -3,7 +3,9 @@ import sys
 
 from common import util
 
+print('Python Version: ', sys.version)
 print("Util Version: ", util.version())
 for var, value in os.environ.items():
-    print(f"{var} = '{value}")
+    if 'python' in var.lower():
+        print(f"{var} = '{value}")
     
