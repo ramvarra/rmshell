08cf857473e220ce59373314053ec831215f9de0
Ram Varra <ramvarra@hotmail.com>
2023-12-03 11:23:43 -0700

    lint changes

--- server/rmserver.py
+++ server/rmserver.py
@@ -1,9 +1,9 @@
+''' Remote Shell Server '''
 import os
-import sys
 
 from common import util
 
-print("Util Version: ", util.version())
-for var, value in os.environ.items():
-    print(f"{var} = '{value}")
-    
+if __name__ == '__main__':
+    print("Util Version: ", util.version())
+    for var, value in os.environ.items():
+        print(f"{var} = '{value}")
